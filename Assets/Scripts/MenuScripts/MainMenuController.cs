96e7cdef78590326d81863585f421ea1c8128e4c
Tomtopo <64419752+Tomtopo@users.noreply.github.com>
2021-09-22 14:36:21 +0300

    Added some color to the menu and made everything better overall. Little changes to the SaveSystem.

--- Assets/Scripts/MenuScripts/MainMenuController.cs
+++ Assets/Scripts/MenuScripts/MainMenuController.cs
@@ -8,12 +8,15 @@ using DG.Tweening;
 public class MainMenuController : MonoBehaviour
 {
     public RectTransform credits;
-    public CanvasGroup fader, buttons;
+    public CanvasGroup fader, buttons, popUp;
     public GameObject settingsMenu;
+    public TMPPlayer saveData;
+    public GameObject warningPopUp;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+        
         //fader.alpha = 1;
         //fader.DOFade(0, 1).SetUpdate(true);
     }
@@ -27,26 +30,49 @@ public class MainMenuController : MonoBehaviour
     //Starts new game file while asking to delete the old one if it exists.
     public void StartGame()
     {
-        //if(nosavedata) {Start game immediately} else {WarningPopUpWindow = active}
-        SceneManager.LoadScene(1);
+        //if(nosavedata) {Start game} else {WarningPopUpWindow = active}
+        if(saveData.CheckData())
+        {
+            buttons
+                .DOFade(0, .3f)
+                .SetUpdate(true);
+
+            warningPopUp.SetActive(true);
+        }
+        else
+        {
+            //Aloitetaan peli alusta.
+            Debug.Log("New game started.");
+        }
+        
     }
 
     //Continues the game from the last save.
     public void ContinueGame()
     {
-
+        saveData.LoadPlayer();
     }
 
     //Button action for deleting all save data.
     public void WarningPopUpYes()
     {
         //Delete all save data here.
+        saveData.DeleteSave();
+        Debug.Log("Save data deleted through main menu.");
+        popUp
+            .DOFade(0, .2f)
+            .SetUpdate(true);
+
+        StartGame();
     }
 
     //Button action for closing the Warning popup for deleting save data.
     public void WarningPopUpNo()
     {
-
+        warningPopUp.SetActive(false);
+        buttons
+            .DOFade(1, .3f)
+            .SetUpdate(true);
     }
 
     //Opens the credits panel
