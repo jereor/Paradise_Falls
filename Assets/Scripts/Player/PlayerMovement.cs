360e419d09906b96d5086e974244bcaf47bb7bdb
jereor <jere.orava@hotmail.com>
2021-09-23 15:57:57 +0300

    Work in progress Coyote Jump mechanic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,11 +10,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementVelocity; // Movement speed variable
     [SerializeField] private float jumpForce; // Jump height variable
     [SerializeField] private bool isFacingRight = true; // Tracks player sprite direction
+    [SerializeField] private float coyoteTime; // Determines coyote time forgiveness
 
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck; // GameObject attached to player that checks if touching ground
     [SerializeField] private float checkRadius; // Radius for ground checks
     [SerializeField] LayerMask groundLayer; // Chosen layer that is recognized as ground in ground checks
+
+    public float coyoteTimer; // Timer for coyote jumps
 
     // References
     private Rigidbody2D rb;
@@ -26,6 +29,10 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        // Timers
+        coyoteTimer += Time.deltaTime;
+
+        // Movement
         rb.velocity = new Vector2(horizontal * movementVelocity, rb.velocity.y); // Moves the player by horizontal input
 
         if (!isFacingRight && horizontal > 0f) // Flip when turning right
@@ -55,8 +62,12 @@ public class PlayerMovement : MonoBehaviour
         horizontal = context.ReadValue<Vector2>().x; // Updates the horizontal input direction
     }
 
+    // Jump action: Called when the Jump Action button is pressed
     public void Jump(InputAction.CallbackContext context) // Context tells the function when the action is triggered
     {
+        //var coyote = coyoteTimer < coyoteTime; // Check if enough time has passed since last coyote jump
+        //if (!IsGrounded() || Time.timeScale != 1) return; // Return if time is stopped so players can't abuse pause menu
+
         if (context.performed && IsGrounded()) // If button was pressed
             rb.velocity = new Vector2(rb.velocity.x, jumpForce); // Keep player in upwards motion
 
