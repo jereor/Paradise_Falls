9c3028952c86abad4dedbca28ccca5b9a4efc118
TJere <56117300+TJere@users.noreply.github.com>
2021-09-27 09:19:31 +0300

    FloatingText to Interact + comments

--- Assets/Scripts/JereT/Interactable.cs
+++ Assets/Scripts/JereT/Interactable.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
 
 public class Interactable : MonoBehaviour
 {
+    [Header("Variables from Interactable script")]
     public UnityEvent itemEvent;
+    public TMP_Text floatingText;
+    [SerializeField] private string textToShow = "Press F to interact";
+    [SerializeField] private string interactedText = "Interacted";
 
     private void Start()
     {
+        // Set text at start
+        floatingText.text = textToShow;
+        // Hide text as default
+        HideFloatingText();
+        // If item event is not set we set new UnityEvent and AddListener to Interact.
+        // Interact() is called in PlayerInteractions when pressed F as a Event with Invoke()
         if (itemEvent == null)
         {
             itemEvent = new UnityEvent();
@@ -15,8 +27,32 @@ public class Interactable : MonoBehaviour
         itemEvent.AddListener(Interact);
     }
 
+    // Makes text object visible 
+    public virtual void ShowFloatingText()
+    {
+        // If text was changed to interactedText and HideFloatingText() was called change text back to textToShow
+        if (!floatingText.text.Equals(textToShow))
+        {
+            floatingText.text = textToShow;     // Press F to interact
+        }
+        floatingText.gameObject.SetActive(true);
+    }
+
+    public virtual void HideFloatingText()
+    {
+        floatingText.gameObject.SetActive(false);
+    }
+
+    // Should be called when Interact functions is Invoked if you wish to show some text after interacting not required
+    public virtual void InteractedTextUpdate()
+    {
+        floatingText.text = interactedText;     // Interacted
+    }
+
+    // All items will have this functions as default override this in items own script to do something. Example save or heal when picked up
     public virtual void Interact()
     {
+        InteractedTextUpdate();
         Debug.Log("Interacted with: " + gameObject.transform.name);
     }
 }
--- Assets/Scripts/Saving/SavePoint.cs
+++ Assets/Scripts/Saving/SavePoint.cs
@@ -3,18 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class SavePoint : Interactable
 {
+    [Header("Variables from This script")]
     [SerializeField] private bool playerIsClose;
+    [SerializeField] private bool saveProtector;    // Checks if player has allready saved game and wont allow save again until leaves proximity
+    [SerializeField] private bool showFloatingText = true;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.name.Contains("Player")) 
         {
+            // Gives persmission to save and gives GameObject that this script is attached so PlayerInteraction know of whos Interact() to Invoke
             collision.GetComponent<PlayerInteractions>().AllowInteract(true);
             collision.GetComponent<PlayerInteractions>().GiveGameObject(gameObject);
+            // Mark for this item that player is close (easier to track interactions when debugging)
             playerIsClose = true;
+
+            if (showFloatingText)
+            {
+                ShowFloatingText();
+            }
         }
     }
 
+    // Reverse of OnTriggerEnter2D()
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.name.Contains("Player"))
@@ -22,26 +33,47 @@ public class SavePoint : Interactable
             collision.GetComponent<PlayerInteractions>().AllowInteract(false);
             collision.GetComponent<PlayerInteractions>().GiveGameObject(null);
             playerIsClose = false;
+            // Player leaves items proximity so we can allow saving again when Player comes back to this item
+            saveProtector = false;
+
+            if (showFloatingText)
+            {
+                HideFloatingText();
+            }
         }
     }
 
     public override void Interact()
     {
-        Debug.Log("Saving game with interaction...");
-
-        // Main Game Scene
-        if (SceneManager.GetActiveScene().name.Contains("01"))
+        // Check if player has saved with this item if not allow save if yes disable saving until Player leaves proximity
+        if (!saveProtector)
         {
-            Level01Loader.levelLoaderInstance.Save();
+            Debug.Log("Saving game with interaction...");
+
+            if (showFloatingText)
+            {
+                InteractedTextUpdate();
+            }
+
+            // We need to find levelLoaderInstance of this scene all scenes should have personalized LevelLoaders (different map aka positions of items and enemies)
+            // Main Game Scene
+            if (SceneManager.GetActiveScene().name.Contains("01"))
+            {
+                Level01Loader.levelLoaderInstance.Save();
+            }
+            if (SceneManager.GetActiveScene().name.Contains("Demo"))
+            {
+                DemoLoader.levelLoaderInstance.Save();
+            }
+            saveProtector = true;
         }
-        if (SceneManager.GetActiveScene().name.Contains("Demo"))
+        else
         {
-            DemoLoader.levelLoaderInstance.Save();
-        }
-
-
+            Debug.Log("Save spamming is not allowed");
+        }      
     }
 
+    // LevelLoader calls this to check where Player is saving the game to set spawnpoint
     public bool getPlayerIsClose()
     {
         return playerIsClose;
