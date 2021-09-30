1d72c66193c671804f3c1f9ddebdeeddaf4f193c
TJere <56117300+TJere@users.noreply.github.com>
2021-09-30 14:56:18 +0300

    Update MeleeWeapon.cs

--- Assets/Scripts/Items/MeleeWeapon.cs
+++ Assets/Scripts/Items/MeleeWeapon.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class MeleeWeapon : Interactable
 {
     [Header("Variables from This script")]
-    [SerializeField] private float weaponThrowDamage;
-    [SerializeField] private float rotAngle;
-    [SerializeField] private float ricochetForce;
-    [SerializeField] private float maxDistance;
+    [SerializeField] private float weaponThrowDamage; // Damage dealt if hits enemy
+    [SerializeField] private float rotAngle; // Rotation angle to spin when thowing
+    [SerializeField] private float ricochetForce; // Force of hit ricochet on enemies and gorund elements
+    [SerializeField] private float maxDistance; // Max distance to travel with gravityscale 0 and deal damage
     [SerializeField] private bool playerIsClose; // ATM used for debugging
-    [SerializeField] private bool isFlying = true;
+    //[SerializeField] private bool isFlying = true;
     private PlayerCombat combatScript;
     private float defaultGravityScale;
     private Vector3 startPoint;
@@ -44,13 +44,12 @@ public class MeleeWeapon : Interactable
             Debug.Log("Flying");
             transform.Rotate(new Vector3(0f,0f, - rotAngle * Time.deltaTime));
         }
-        if((transform.position - startPoint).magnitude >= maxDistance && !beingPulled)
+        if ((transform.position - startPoint).magnitude >= maxDistance && !beingPulled)
         {
             Debug.Log("Max d");
             myRB.gravityScale = defaultGravityScale;
             canDealDamage = false;
         }
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -80,22 +79,24 @@ public class MeleeWeapon : Interactable
                 Vector2 tmp = new Vector2(transform.position.x, transform.position.y) - collision.GetContact(0).point;
                 myRB.AddForce(tmp.normalized * ricochetForce, ForceMode2D.Impulse);
             }
-
-            isFlying = false;
+            else
+            {
+                canDealDamage = false;
+            }
         }
         // Enemy
         else if (collision.gameObject.layer == 7)
         {          
+            // Hits enemy when flying and can deal damage
             if (canDealDamage)
             {
                 collision.gameObject.GetComponent<Health>().TakeDamage(weaponThrowDamage);
+
                 Debug.Log("Bonk " + collision.gameObject.name);
                 myRB.gravityScale = defaultGravityScale;
                 
                 myRB.AddForce((transform.position - collision.gameObject.transform.position).normalized + new Vector3(0,2,0) * ricochetForce, ForceMode2D.Impulse);
             }
-            
-            isFlying = false;
         }
     }
 
@@ -111,6 +112,7 @@ public class MeleeWeapon : Interactable
         }
     }
 
+    // Called from PlayerMeleeCombat
     public void PullWeapon(GameObject objectThatPulls)
     {
         if (!canDealDamage)
@@ -123,6 +125,7 @@ public class MeleeWeapon : Interactable
         }
     }
 
+    // Called as event from player if interacted or when weapon is pulled and hits player
     public override void Interact()
     {
         Debug.Log("Pick up");
