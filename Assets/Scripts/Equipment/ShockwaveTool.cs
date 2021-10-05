20fa04c7cb617d1e73a561567b33941e87b155f2
jereor <jere.orava@hotmail.com>
2021-10-05 15:49:05 +0300

    Testing out how to do Shockwave direction.

--- Assets/Scripts/Equipment/ShockwaveTool.cs
+++ Assets/Scripts/Equipment/ShockwaveTool.cs
@@ -12,10 +12,28 @@ public class ShockwaveTool : MonoBehaviour
     [SerializeField] private float usageCooldown;
 
     [Header("References")]
+    [SerializeField] private Player playerScript;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private GameObject shockwaveDirection;
     [SerializeField] private ParticleSystem shockwaveEffect;
 
     private float nextShockwave = -1; // Initialize as -1. First time it will always be less than the current time so use it can be used. 
+
+    private void Update()
+    {
+        UpdateDirection();    
+    }
+
+    private void UpdateDirection()
+    {
+        var vertical = playerScript.InputVertical;
+
+        switch (vertical)
+        {
+            case -1: shockwaveDirection.transform.localPosition = new Vector3(-1, 1, 0); break; // DIVE
+            case 1: shockwaveDirection.transform.localPosition = new Vector3(-1, -1, 0); break; // JUMP
+        }
+    }
 
     // Shockwave action: Called when the Shockwave Action Button is pressed
     public void Shockwave(InputAction.CallbackContext context) // Context tells the function when the action is triggered
@@ -24,7 +42,7 @@ public class ShockwaveTool : MonoBehaviour
         {
             nextShockwave = Time.time + usageCooldown;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            //shockwaveEffect.Play();
+            shockwaveEffect.Play();
             StartCoroutine(ActivateCooldown(usageCooldown));
         }
     }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float InputHorizontal { get; private set; }
+    public float InputVertical { get; private set; }
+
     private GUIStyle TextStyleEnergy = new GUIStyle();
     private GUIStyle TextStyleHealth = new GUIStyle();
     // This script can be used to control all player sub scripts through one place.
-    // Not in use right now because the new Unity Input System does all the work for us. (Input does not need to be tracked separately)
+    // Holds player components and state variables that can be accessed from anywhere
 
     // This script could maybe become useful when starting to make animation controller. 
     // But maybe even then a separate animation controller script could be enough by itself.
@@ -39,5 +43,14 @@ public class Player : MonoBehaviour
     {
         GUI.Label(new Rect(10, 10, 300, 100), "Health: " + healthScript.GetHealth(), TextStyleHealth);
         GUI.Label(new Rect(200, 10, 300, 100), "Energy: " + energyScript.GetEnergy(), TextStyleEnergy);
+        GUI.Label(new Rect(400, 10, 300, 100), "Horizontal: " + InputHorizontal, TextStyleHealth);
+        GUI.Label(new Rect(600, 10, 300, 100), "Vertical: " + InputVertical, TextStyleEnergy);
+    }
+
+    // Move action: Called when the Move Action Button is pressed
+    public void Move(InputAction.CallbackContext context) // Context tells the function when the action is triggered
+    {
+        InputHorizontal = Mathf.Round(context.ReadValue<Vector2>().x); // Updates the horizontal input direction
+        InputVertical = Mathf.Round(context.ReadValue<Vector2>().y);// Updates the vertical input direction
     }
 }
