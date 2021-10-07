e813ada51777e6dabb244ecf4af02c2bb8357708
TJere <56117300+TJere@users.noreply.github.com>
2021-10-07 12:31:13 +0300

    Revamp of combosystem
    Placeholder coroutines as attack animation.
    Attack animation know its combo index and calls DealDamage() with parameters and DealDamage() deals damage with given information from animation.

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -11,11 +11,6 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private float knockbackForceNormal;
     [SerializeField] private float knockbackForceLast;
 
-
-    [SerializeField] private float meleeAttackRate; // Time between combo attacks
-    [SerializeField] private float comboCooldown; // Timer of when we need to start new combo
-    [SerializeField] private float comboBetweenTimer; // If this runs out we set combo on cooldown
-
     [Header("Throwing")]
     [SerializeField] private GameObject throwIndicator; // Object used to rotate throwPoint and pointPrefabs
     [SerializeField] private Transform throwPoint; // Point where the weapon will be Instantiated
@@ -45,11 +40,7 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private bool isWeaponWielded;
 
     // State variables
-    //private float? meleeButtonPressedTime;
-    private float comboEndTime = 0; // Set here to zero so we are sure it starts from zero
-    private float lastTimeMeleed = 0;
     private bool throwAim; // True if we are holdling Throw input
-    private int currentComboHit = 1; // Combo counter 1 normal, 2 normal, 3 last hit -> comboCooldown -> Combo can be done again
 
     private bool heavyComboHold;
 
@@ -57,27 +48,35 @@ public class PlayerCombat : MonoBehaviour
     private float throwChargeStartTime; // Time when we start charging
     private float ratio; // Ratio float to grow desired parameters in same ratio. "One ratio to rule them all"
 
-    private bool comboOnCooldown; // boolean to check if we can attack
-    private bool comboOnGoing; // Used in ComboCounter() calculates if we aren't attacking before comboBetweenTimer then we reset combo
-
     Mouse mouse = Mouse.current; // Mouse in use on Unity player
 
     Camera mainCamera;  // MainCamera
     Ray mousePosRay; // Ray from MainCamera to mouse position in screen (endpoint aka .origin vector is what we can use)
 
-    public GameObject meleeDebugParticles;
-
     Vector2 vectorToTarget;
 
-    Coroutine meleeAnimation;
+    public bool onIdle = true;
+    public bool onTran1 = false;
+    public bool onTran2 = false;
+    public bool onTran3 = false;
 
-    Coroutine comboBuffer;
+    Coroutine tranToIdle;
+
+    public static PlayerCombat instance;
+    public bool canReceiveInput;
+    public bool inputReceived;
+
+    private void Awake()
+    {
+        instance = this;
+        canReceiveInput = true;
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        // We don't have to wait cooldown on start
-        comboEndTime = comboEndTime - comboCooldown;
         // MainCamera
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
@@ -86,9 +85,62 @@ public class PlayerCombat : MonoBehaviour
         InitPoints();
     }
 
+    // PLACEHOLDER ANIMATOR STATES
     private void Update()
     {
-        
+        // Idle
+        if (onIdle && inputReceived)
+        {
+            Debug.Log("Attack1");
+            StartCoroutine(PlaceHolderAttack1());
+            InputManager();
+            onIdle = false;
+            inputReceived = false;
+        }
+
+        if (onTran1 && inputReceived)
+        {
+            Debug.Log("Attack2");
+            StopCoroutine(tranToIdle);
+            StartCoroutine(PlaceHolderAttack2());
+            InputManager();
+            onTran1 = false;
+            inputReceived = false;
+        }
+
+        if (onTran2 && inputReceived)
+        {
+            Debug.Log("Attack3");
+            StopCoroutine(tranToIdle);
+            StartCoroutine(PlaceHolderAttack3());
+            InputManager();
+            onTran2 = false;
+            inputReceived = false;
+        }
+    }
+
+    IEnumerator TranToIdle(int tranI)
+    {
+        Debug.Log("Transition enter");
+        yield return new WaitForSecondsRealtime(1f);
+        Debug.Log("Transition exit");
+        if (tranI == 1)
+            onTran1 = false;
+        else if (tranI == 2)
+            onTran2 = false;
+        else if (tranI == 3)
+            onTran3 = false;
+
+
+        gameObject.GetComponent<PlayerMovement>().enabled = true;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 5f;
+
+        onIdle = true;
+        if (!canReceiveInput)
+        {
+            InputManager();
+        }
+        inputReceived = false;
     }
 
     private void FixedUpdate()
@@ -99,8 +151,6 @@ public class PlayerCombat : MonoBehaviour
             numberOfPoints = (int)maxDistance;
             InitPoints();
         }
-
-        ComboCounter();
 
         RotateIndicator();
 
@@ -116,23 +166,23 @@ public class PlayerCombat : MonoBehaviour
         // Start Throwing
         if (context.performed && isWeaponWielded && throwAim && meleeWeaponPrefab)
         {
+            Debug.Log("Throw init");
             // Set time here since we start charging
             throwButtonPressedTime = Time.time;
         }
 
         // Normal Melee
-        else if (context.performed && isWeaponWielded && CheckAttackRate() && !throwAim && !comboOnCooldown)
+        else if (context.performed && isWeaponWielded && !throwAim && canReceiveInput)
         {
-            // We start combo if we havent one started
-            if(!comboOnGoing)
-                comboOnGoing = true;
-
-            // !!!!! When melee animations are here do this instead: Run correct animation here with currentCombo, when animation visually hits run DealDamage(),
-            if(meleeAnimation == null)
-                meleeAnimation = StartCoroutine(PlaceHolderMeleeAnimation());
+            Debug.Log("Input");
+            inputReceived = true;
+            canReceiveInput = false;
         }
+
+
+
         // Pull weapon if thrown
-        else if(context.performed && !isWeaponWielded && CheckAttackRate() && weaponInstance != null)
+        else if(context.performed && !isWeaponWielded && weaponInstance != null)
         {
             Debug.Log("Trying to pull weapon");
             weaponInstance.GetComponent<MeleeWeapon>().PullWeapon(gameObject);          
@@ -206,34 +256,7 @@ public class PlayerCombat : MonoBehaviour
 
     // --- MELEE ---
 
-    private void ComboCounter()
-    {
-        // If we can start new combo
-        if (Time.time - comboEndTime > comboCooldown)
-        {
-            // Combo is not on cooldown anymore
-            if (comboOnCooldown)
-                comboOnCooldown = false;
-
-            // Check if we need to reset combo due no attacks
-            if (comboOnGoing)
-            {
-                // We dont melee between lastTimeMeleed and lastTimeMeleed + comboBetweenTimer
-                if (Time.time - lastTimeMeleed > comboBetweenTimer)
-                {
-                    // No attacks 
-                    ResetCombo();
-                }
-            }
-        }
-    }
-
-    private bool CheckAttackRate()
-    {
-        return (Time.time - lastTimeMeleed >= meleeAttackRate);
-    }
-
-    IEnumerator PlaceHolderMeleeAnimation()
+    IEnumerator PlaceHolderAttack1()
     {
         //Debug.Log("Started melee animation");
         gameObject.GetComponent<PlayerMovement>().enabled = false;
@@ -242,44 +265,76 @@ public class PlayerCombat : MonoBehaviour
 
         yield return new WaitForSecondsRealtime(1f);
 
-        Debug.Log("Ended melee animation");
+        Debug.Log("Ended melee animation 1");
 
-        DealDamage();
-        if (comboBuffer != null)
-        {
-            StopCoroutine(comboBuffer);
-            //comboBuffer = null;
-            comboBuffer = StartCoroutine(PlaceHolderComboBuffer());
-        }
-        else
-        {
-            comboBuffer = StartCoroutine(PlaceHolderComboBuffer());
-        }
+        DealDamage(1, false);
 
-        meleeAnimation = null;
+        canReceiveInput = true;
+        onTran1 = true;
+
+        tranToIdle = StartCoroutine(TranToIdle(1));
     }
 
-    IEnumerator PlaceHolderComboBuffer()
+    IEnumerator PlaceHolderAttack2()
     {
+        //Debug.Log("Started melee animation");
+        gameObject.GetComponent<PlayerMovement>().enabled = false;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
 
         yield return new WaitForSecondsRealtime(1f);
 
-        gameObject.GetComponent<PlayerMovement>().enabled = true;
-        gameObject.GetComponent<Rigidbody2D>().gravityScale = 5f;
+        Debug.Log("Ended melee animation 2");
+
+        DealDamage(2, false);
+
+        canReceiveInput = true;
+        onTran2 = true;
+
+        tranToIdle = StartCoroutine(TranToIdle(2));
+    }
+
+    IEnumerator PlaceHolderAttack3()
+    {
+        //Debug.Log("Started melee animation");
+        gameObject.GetComponent<PlayerMovement>().enabled = false;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0f;
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        yield return new WaitForSecondsRealtime(1f);
+
+        Debug.Log("Ended melee animation 3");
+
+        DealDamage(3, false);
+
+        onTran3 = true;
+
+        tranToIdle = StartCoroutine(TranToIdle(3));
+    }
+
+    public void InputManager()
+    {
+        if (!canReceiveInput)
+        {
+            canReceiveInput = true;
+        }
+        else
+        {
+            canReceiveInput = false;
+        }
     }
 
     // Call this function on melee animation end when melee visually hits something
-    public void DealDamage()
+    public void DealDamage(int attackI, bool heavyHit)
     {
-        Instantiate(meleeDebugParticles, attackPoint.position, Quaternion.identity);
-
         // Draws a box in scene if objects from enemyLayer overlap with this box store them in hitEnemies array
         Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(attackPoint.position, new Vector2(attackRangeX, attackRangeY), 0f, enemyLayer);
 
+        Debug.Log("DEALING DMG combo hit: " + attackI + " Heavy?: " + heavyHit);
+
         // Normal combo hits 1 and 2
-        if (currentComboHit < 3 && !heavyComboHold)
+        if (attackI < 3 && !heavyHit)
         {
-            Debug.Log("Current combohit: " + currentComboHit + " Heavy?: " + heavyComboHold);
             // If there is elements on hitEnemies array go through it
             foreach (Collider2D enemy in hitEnemies)
             {
@@ -294,14 +349,10 @@ public class PlayerCombat : MonoBehaviour
                     //Knockback(enemy.gameObject, gameObject, knockbackForceNormal);
                 }
             }
-
-            currentComboHit++;
         }
         // Normal combo hit 3 aka last hit of combo
-        else if (currentComboHit == 3 && !heavyComboHold)
+        else if (attackI == 3 && !heavyHit)
         {
-            Debug.Log("Current combohit (last): " + currentComboHit + " Heavy?: " + heavyComboHold);
-
             // If there is elements on hitEnemies array go through it
             foreach (Collider2D enemy in hitEnemies)
             {
@@ -314,13 +365,10 @@ public class PlayerCombat : MonoBehaviour
                     Knockback(enemy.gameObject, gameObject, knockbackForceLast);
                 }
             }
-
-            ResetCombo();
         }
         // Heavy combo hits 1 and 2
-        if (currentComboHit < 3 && heavyComboHold)
+        if (attackI < 3 && heavyHit)
         {
-            Debug.Log("Current combohit: " + currentComboHit + " Heavy?: " + heavyComboHold);
             // If there is elements on hitEnemies array go through it
             foreach (Collider2D enemy in hitEnemies)
             {
@@ -333,14 +381,10 @@ public class PlayerCombat : MonoBehaviour
                     Knockback(enemy.gameObject, gameObject, knockbackForceNormal);
                 }
             }
-
-            currentComboHit++;
         }
         // Heavy combo hit 3 aka last hit of combo
-        else if (currentComboHit == 3 && heavyComboHold)
+        else if (attackI == 3 && heavyHit)
         {
-            Debug.Log("Current combohit (last): " + currentComboHit + " Heavy?: " + heavyComboHold);
-
             // If there is elements on hitEnemies array go through it
             foreach (Collider2D enemy in hitEnemies)
             {
@@ -353,27 +397,13 @@ public class PlayerCombat : MonoBehaviour
                     Knockback(enemy.gameObject, gameObject, knockbackForceLast);
                 }
             }
-
-            ResetCombo();
         }
-
-
-        lastTimeMeleed = Time.time;
     }
 
     // Debug info draws hit point in this case cube
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireCube(attackPoint.position, new Vector3(attackRangeX, attackRangeY, 0f));
-    }
-
-    // Sets correct variables to disable melee attacks
-    private void ResetCombo()
-    {
-        comboOnCooldown = true;
-        comboOnGoing = false;
-        currentComboHit = 1;
-        comboEndTime = Time.time;
     }
 
     // Small knockback to the target when too close to the enemy unit. Knockback knocks slightly upwards so the friction doesn't stop the target right away.
