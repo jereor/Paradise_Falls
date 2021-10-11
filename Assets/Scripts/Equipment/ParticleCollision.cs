b569cea1f65f629e39c29e80a99ba73fadf533f6
jereor <jere.orava@hotmail.com>
2021-10-11 15:25:27 +0300

    Knockbacks improved.

--- Assets/Scripts/Equipment/ParticleCollision.cs
+++ Assets/Scripts/Equipment/ParticleCollision.cs
@@ -5,13 +5,29 @@ using Cinemachine;
 
 public class ParticleCollision : MonoBehaviour
 {
+    [SerializeField] float knockbackForce;
+
+    private ParticleSystem ps;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
     private void OnParticleCollision(GameObject collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
+            var numCollisionEvents = ps.GetCollisionEvents(collision, collisionEvents);
+            var direction = collision.transform.position - transform.position;
+
+            collision.GetComponent<Rigidbody2D>().AddForceAtPosition(direction.normalized * knockbackForce, collisionEvents[0].intersection, ForceMode2D.Impulse);
+
             if (collision.TryGetComponent(out GroundEnemyAI enemyScript))
             {
-                enemyScript.KnockBackEnemy(this.gameObject, 300);
+                //enemyScript.KnockBackEnemy(transform.gameObject , knockbackForce);
             }
 
         }
