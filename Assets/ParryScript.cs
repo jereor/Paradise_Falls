90c3ff019b58275ccae9e83ea750d35c6b3a1d2b
TJere <56117300+TJere@users.noreply.github.com>
2021-10-15 10:27:07 +0300

    Removed debugs

--- Assets/ParryScript.cs
+++ Assets/ParryScript.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class ParryScript : StateMachineBehaviour
 {
-
-    private float time;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Player.Instance.SetCurrentState(Player.State.Parrying);
         animator.SetBool("isParrying", true);
-        time = Time.time;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -21,11 +18,10 @@ public class ParryScript : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        Debug.Log("Clip " + (Time.time - time));
-        //animator.SetBool("isParrying", false);
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
