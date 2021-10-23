da4f2ee3044ffd78c4ac455e5602a9c61814e55d
jereor <jere.orava@hotmail.com>
2021-10-23 12:18:48 +0300

    Testing some methods to make camera transitions flawless.

--- Assets/Scripts/Camera/TransitionTrigger.cs
+++ Assets/Scripts/Camera/TransitionTrigger.cs
@@ -7,12 +7,45 @@ public class TransitionTrigger : MonoBehaviour
     [Header("References")]
     [SerializeField] private GameObject newCamera; // new camera attached in this object's inspector
 
+    [Header("Cooldown")]
+    [SerializeField] private float transitionCooldown;
+
+    // References
+    private GameObject currentCamera;
+
+    // State variables
+    private bool inTransition;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // If collision with player, switch camera to the new camera
         if (collision.CompareTag("Player"))
         {
-            CameraTransitions.Instance.SwitchCameras(newCamera);
+            currentCamera = CameraTransitions.Instance.GetCurrentCamera();
+            if (currentCamera != newCamera) // New camera is different than the current camera
+            {
+                CameraTransitions.Instance.SwitchCameras(newCamera);
+                StartCoroutine(ActivateTransitionCD());
+            }
+            //if (currentCamera != newCamera && !inTransition) // New camera is different and camera is currently not in transition
+            //{
+            //    CameraTransitions.Instance.SwitchCameras(newCamera);
+            //    StartCoroutine(ActivateTransitionCD());
+            //}
         }
+    }
+
+    private IEnumerator ActivateTransitionCD()
+    {
+        inTransition = true;
+
+        float transitionTimer = 0;
+        while (transitionTimer <= transitionCooldown)
+        {
+            transitionTimer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        inTransition = false;
     }
 }
