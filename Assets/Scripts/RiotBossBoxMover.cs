633dbd19d431acffb5b8e42eaf7300b4c5a55125
Tomtopo <64419752+Tomtopo@users.noreply.github.com>
2021-10-26 15:37:15 +0300

    BoxMover script overhaul. User can determine points where the box will move. Various bools to decide the behaviour.

--- Assets/Scripts/RiotBossBoxMover.cs
+++ Assets/Scripts/RiotBossBoxMover.cs
@@ -7,33 +7,101 @@ public class RiotBossBoxMover : MonoBehaviour
 {
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider;
-    [SerializeField] private float speed;
     [SerializeField] private float firstStepTime;
-    [SerializeField] private float firstWaitTime;
-    [SerializeField] private float secondStepTime;
     [SerializeField] private float secondWaitTime;
-    [SerializeField] private float thirdStepTime;
-    [SerializeField] private float moveLength;
+    [SerializeField] private Vector2[] moves;
+    private Vector2 startPosition;
+    [SerializeField] private int stepCounter = 0;
+
 
     private bool changeState = false;
+    private bool canStep = true;
+    private bool returning = false;
+    private bool shutScript = false;
+
     private bool isWaiting = false;
     private bool isChainCut = false;
+    [SerializeField] private bool colliderDisabledAtStart = false;
+    [SerializeField] private bool comesAndGoesFromBackground = false;
+    private bool gizmoPositionChange = true;
 
-    [SerializeField] private Mover state;
+    [SerializeField] private bool cuttableChain = false;
+    [SerializeField] private bool loop = true;
+    [SerializeField] private bool destroyAfterPathComplete = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
+
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
-        state = Mover.FirstStep;
+        gizmoPositionChange = false;
+        startPosition = transform.position;
+        if(colliderDisabledAtStart)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
+            boxCollider.enabled = !boxCollider.enabled;
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(gizmoPositionChange)
+        {
+            Gizmos.color = Color.red;
+            if (moves[0] != null)
+            {
+                Vector2 position;
+                Gizmos.DrawLine((Vector2)transform.position, (Vector2)transform.position + moves[0]);
+                Gizmos.DrawSphere((Vector2)transform.position + moves[0], 0.5f);
+                position = (Vector2)transform.position + moves[0];
+                for (int i = 1; i < moves.Length; i++)
+                {
+
+                    Gizmos.DrawLine(position, position + moves[i]);
+                    Gizmos.DrawSphere(position, 0.4f);
+                    position += moves[i];
+                }
+                if(loop)
+                {
+                    Gizmos.DrawLine(position, transform.position);
+                    Gizmos.DrawSphere(position, 0.4f);
+                    Gizmos.DrawSphere(transform.position, 0.4f);
+                }
+            }
+        }
+        else
+        {
+            Gizmos.color = Color.red;
+            if (moves[0] != null)
+            {
+                Vector2 position;
+                Gizmos.DrawLine(startPosition, startPosition + moves[0]);
+                Gizmos.DrawSphere(startPosition + moves[0], 0.5f);
+                position = startPosition + moves[0];
+                for (int i = 1; i < moves.Length; i++)
+                {
+
+                    Gizmos.DrawLine(position, position + moves[i]);
+                    Gizmos.DrawSphere(position, 0.4f);
+                    position += moves[i];
+                }
+                if (loop)
+                {
+                    Gizmos.DrawLine(position, startPosition);
+                    Gizmos.DrawSphere(position, 0.4f);
+                    Gizmos.DrawSphere(startPosition, 0.4f);
+                }
+            }
+        }
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log(state);
-        if(gameObject.transform.childCount == 0 && !isChainCut)
+        if(gameObject.transform.childCount == 0 && !isChainCut && cuttableChain)
         {
             Debug.Log("test");
             isChainCut = true;
@@ -44,109 +112,16 @@ public class RiotBossBoxMover : MonoBehaviour
             rb.constraints = RigidbodyConstraints2D.None;
             rb.constraints = RigidbodyConstraints2D.FreezePositionX;
             rb.freezeRotation = true;
-            state = Mover.Idle;
         }
-
-        switch (state)
+        if(!shutScript)
         {
-            case Mover.Idle:
-                break;
+            if (canStep && !returning)
+                HandleStep();
+            else if (!canStep && !returning)
+                HandleWait();
 
-            case Mover.FirstStep:
-                HandleFirstStep();
-                break;
-
-            case Mover.FirstWait:
-                HandleFirstWait();
-                break;
-
-            case Mover.SecondStep:
-                HandleSecondStep();
-                break;
-
-            case Mover.SecondWait:
-                HandleSecondWait();
-                break;
-
-            case Mover.ThirdStep:
-                HandleThirdStep();
-                break;
-        }
-
-    }
-
-    private void HandleFirstStep()
-    {
-        if(!isWaiting && !changeState)
-        {
-            Move(moveLength, firstStepTime);
-            StartCoroutine(Wait(firstStepTime));
-        }
-        if (changeState)
-        {
-            state = Mover.FirstWait;
-            changeState = false;
-        }
-
-    }
-
-    private void HandleFirstWait()
-    {
-        if (!isWaiting && !changeState)
-        {
-            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
-            EnableDisableBoxCollider();
-            StartCoroutine(Wait(firstWaitTime));
-        }
-        if (changeState)
-        {
-
-            state = Mover.SecondStep;
-            changeState = false;
-        }
-    }
-
-    private void HandleSecondStep()
-    {
-        if (!isWaiting && !changeState)
-        {
-            Move(11, secondStepTime);
-            StartCoroutine(Wait(secondStepTime));
-        }
-        if (changeState)
-        {
-            state = Mover.SecondWait;
-            changeState = false;
-        }
-    }
-
-    private void HandleSecondWait()
-    {
-        if (!isWaiting && !changeState)
-        {
-            gameObject.GetComponent<SpriteRenderer>().color = Color.grey;
-            EnableDisableBoxCollider();
-            StartCoroutine(Wait(secondWaitTime));
-        }
-        if (changeState)
-        {
-
-            state = Mover.ThirdStep;
-            changeState = false;
-        }
-    }
-
-    private void HandleThirdStep()
-    {
-        if (!isWaiting && !changeState)
-        {
-            Move(moveLength, thirdStepTime);
-            StartCoroutine(Wait(thirdStepTime));
-        }
-        if (changeState)
-        {
-            changeState = false;
-            Destroy(gameObject);
+            if (returning)
+                HandleReturnStep();
         }
     }
 
@@ -158,26 +133,16 @@ public class RiotBossBoxMover : MonoBehaviour
         changeState = true;
     }
 
-    private void Move(float moveValue, float time)
+    private void Move(Vector2 move, float time)
     {
         //rb.AddForce(new Vector2(1 * speed * Time.deltaTime, 0));
-        rb.DOMove(new Vector2(transform.position.x + moveValue, transform.position.y), time);
+        rb.DOMove((Vector2)transform.position + move, time);
+        stepCounter++;
     }
 
     private void EnableDisableBoxCollider()
     {
         boxCollider.enabled = !boxCollider.enabled;
-    }
-
-    public enum Mover
-    {
-        Idle,
-        FirstStep,
-        FirstWait,
-        SecondStep,
-        SecondWait,
-        ThirdStep
-
     }
 
     public bool getIsBoxColliderEnabled()
@@ -191,6 +156,81 @@ public class RiotBossBoxMover : MonoBehaviour
         if (collision.gameObject.tag == "Boss")
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void HandleWait()
+    {
+        if (!isWaiting && !changeState)
+        {
+
+
+            if (comesAndGoesFromBackground)
+            {
+                if (boxCollider.enabled)
+                    gameObject.GetComponent<SpriteRenderer>().color = Color.grey;
+                else
+                    gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+
+                EnableDisableBoxCollider();
+            }
+
+            StartCoroutine(Wait(secondWaitTime));
+        }
+        if (changeState && stepCounter == moves.Length && !returning)
+        {
+            if (destroyAfterPathComplete)
+                Destroy(gameObject);
+            else if (loop)
+            {
+                changeState = false;
+                returning = true;
+            }
+            else
+                shutScript = true;
+
+        }
+        else if (changeState)
+        {
+            returning = false;
+            changeState = false;
+            canStep = true;
+
+        }          
+    }
+
+    private void HandleStep()
+    {
+        if (!isWaiting && !changeState)
+        {
+            if (moves[stepCounter] == null)
+            {
+                return;
+            }
+            Move(moves[stepCounter], firstStepTime);
+            StartCoroutine(Wait(firstStepTime));
+        }
+        if (changeState)
+        {
+
+            changeState = false;
+            canStep = false;
+        }
+    }
+
+    private void HandleReturnStep()
+    {
+        if (!isWaiting && !changeState)
+        {
+            rb.DOMove(startPosition, firstStepTime);
+            StartCoroutine(Wait(firstStepTime));
+        }
+        if (changeState)
+        {
+            returning = false;
+            canStep = false;
+            stepCounter = 0;
+            changeState = false;
         }
     }
 }
