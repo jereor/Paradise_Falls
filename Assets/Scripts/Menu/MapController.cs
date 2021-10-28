673afcf722dde2858305db51b40656f5dd456432
TJere <56117300+TJere@users.noreply.github.com>
2021-10-28 08:49:31 +0300

    Button holding for zoom + bugfixes

--- Assets/Scripts/Menu/MapController.cs
+++ Assets/Scripts/Menu/MapController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using UnityEngine.EventSystems;
 
 public class MapController : MonoBehaviour
 {
@@ -21,9 +20,16 @@ public class MapController : MonoBehaviour
     [SerializeField] private float minScale;
     [Tooltip("How much scale is multiplied per scroll")]
     [SerializeField] private float increment;
+
+    [Header("Controller usage variables")]
+    [Tooltip("Time how often zoom happends if controller zoom button is held down")]
+    [SerializeField] private float controllerIncrementTimer; 
+    private float controllerIncrementCounter = 0f;
     [SerializeField] private float navigationSpeed;
 
-    private Vector2 navigationMovement;
+    // Controller needs these since behaviour differs from mouse button and scroll
+    private Vector2 controllerNavigationInput;
+    private float controllerZoomInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,22 +39,81 @@ public class MapController : MonoBehaviour
     private void Update()
     {
         // Negative since anchor is right upper corner
-        mapImage.rectTransform.anchoredPosition -= navigationMovement;
+        mapImage.rectTransform.anchoredPosition -= controllerNavigationInput;
+
+        // Controller zoom done in Update to enable button holding down zooming
+        if (controllerIncrementCounter >= controllerIncrementTimer)
+            ControllerZoom();
+        else
+            controllerIncrementCounter += Time.deltaTime;
+
     }
 
     // Bases on https://forum.unity.com/threads/zoom-in-out-on-scrollrect-content-image.284655/ -rainbowegg
     // Modified to work on new input system
     public void Zoom(InputAction.CallbackContext context)
     {
-        //Debug.Log("Zoom " + context.ReadValue<Vector2>().normalized.y);
-        // If value is something else than zero since scrolling calls this function with scroll (- or +)value and scroll stop 0
-        if (context.ReadValue<Vector2>().normalized.y != 0)
+        // Input comes from controller
+        if (context.control.device.displayName.Contains("Controller"))
         {
-            if (Mathf.Sign(context.ReadValue<Vector2>().normalized.y) == -1)
-                currentScale -= increment;
-            else if (Mathf.Sign(context.ReadValue<Vector2>().normalized.y) == 1)
-                currentScale += increment;
+            // Update controllerZoomInput to be used in ControllerZoom()
+            if (context.ReadValue<Vector2>().normalized.y != 0)
+                controllerZoomInput = Mathf.Sign(context.ReadValue<Vector2>().normalized.y);
+            else
+                controllerZoomInput = 0f;
         }
+        // Input comes from mouse
+        else
+        {
+            // If value is something else than zero since scrolling calls this function with scroll (- or +)value and scroll stop 0
+            if (context.ReadValue<Vector2>().normalized.y != 0)
+            {
+                if (Mathf.Sign(context.ReadValue<Vector2>().normalized.y) == -1)
+                    currentScale -= increment;
+                else if (Mathf.Sign(context.ReadValue<Vector2>().normalized.y) == 1)
+                    currentScale += increment;
+            }
+            // Max scaling (cannot go over maxScale value)
+            if (currentScale >= maxScale)
+            {
+                currentScale = maxScale;
+            }
+            // Min scaling (cannot go over minScale value)
+            else if (currentScale <= minScale)
+            {
+                currentScale = minScale;
+            }
+            // We can use x value since both x and y values scale with same increment value to preserve image resolution
+            float oldLocalScaleX = mapImage.rectTransform.localScale.x;
+
+            // Scale image
+            mapImage.rectTransform.localScale = new Vector2(currentScale, currentScale);
+
+            // Move image slightly to zoom in the middle of view 
+            // If not done image "moves" towards origin point we lose the image we are currently viewing
+            mapImage.rectTransform.anchoredPosition *= mapImage.rectTransform.localScale.x / oldLocalScaleX;
+        }
+    }
+
+    public void Navigate(InputAction.CallbackContext context)
+    {
+        // Navigate in Update()
+        controllerNavigationInput = new Vector2(Mathf.Round(context.ReadValue<Vector2>().x),Mathf.Round(context.ReadValue<Vector2>().y)) * navigationSpeed;
+    }
+
+    // Run every time controllerIncrementCounter is >= controllerIncrementTimer so zoom is not too fast(zoomed every Update)
+    // Function enables holding button down to zoom
+    private void ControllerZoom()
+    {
+        // If value is something else than zero since scrolling calls this function with scroll (- or +)value and scroll stop 0
+        if (controllerZoomInput == 0)
+            return;
+
+        if (controllerZoomInput == -1)
+            currentScale -= increment;
+        else if (controllerZoomInput == 1)
+            currentScale += increment;
+
         // Max scaling (cannot go over maxScale value)
         if (currentScale >= maxScale)
         {
@@ -69,25 +134,8 @@ public class MapController : MonoBehaviour
         // If not done image "moves" towards origin point we lose the image we are currently viewing
         mapImage.rectTransform.anchoredPosition *= mapImage.rectTransform.localScale.x / oldLocalScaleX;
 
-    }
-    Vector2 navVector;
-    public void Navigate(InputAction.CallbackContext context)
-    {
-        // If value is something else than zero since scrolling calls this function with scroll (- or +)value and scroll stop 0
-        //if (context.ReadValue<Vector2>().normalized.y != 0)
-        //{
-
-        navigationMovement = new Vector2(Mathf.Round(context.ReadValue<Vector2>().x),Mathf.Round(context.ReadValue<Vector2>().y)) * navigationSpeed;
-            //    if (Mathf.Sign(context.ReadValue<Vector2>().y) == -1)
-            //        mapImage.rectTransform.anchoredPosition = new Vector2(0f, -navigationSpeed) * increment;
-            //    else if (Mathf.Sign(context.ReadValue<Vector2>().y) == 1)
-            //        mapImage.rectTransform.anchoredPosition = new Vector2(0f, navigationSpeed) * increment;
-
-            //    if (Mathf.Sign(context.ReadValue<Vector2>().x) == -1)
-            //        mapImage.rectTransform.anchoredPosition = new Vector2(-navigationSpeed, 0f) * increment;
-            //    else if (Mathf.Sign(context.ReadValue<Vector2>().x) == 1)
-            //        mapImage.rectTransform.anchoredPosition = new Vector2(navigationSpeed, 0f) * increment;
-        //}
+        // Reset counter
+        controllerIncrementCounter = 0f;
     }
 
     // Called from input map button (or pause map button)
