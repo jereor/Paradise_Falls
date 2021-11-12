f89fee0e55ba1ac3bdfb252ab6b64d7f926deb5d
TJere <56117300+TJere@users.noreply.github.com>
2021-11-12 09:45:26 +0200

    New prefabs to be used and mixmatched

--- Assets/ConveyorBelt.cs
+++ Assets/ConveyorBelt.cs
@@ -4,20 +4,48 @@ public class ConveyorBelt : MonoBehaviour
 {
     [SerializeField] private float speed;
 
-    [Header("Collider from sprites")]
+    [Header("Used if button/lever can shut belt off")]
+    [Tooltip("Collider from last right sprite")]
     [SerializeField] private Collider2D rightEndPointCollider; // Only one will be enough
 
     [SerializeField] private Animator[] animators;
 
     private bool stopped = false;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Allows player to jump if landed on last part of belt
+        if (collision.gameObject.CompareTag("Player") && !stopped)
+            rightEndPointCollider.gameObject.layer = LayerMask.NameToLayer("Ground");
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
         if ((collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("MeleeWeapon")) && !stopped)
         {
             // Move collided object with set speed using parents locals scale to change direction smoothly
-            collision.transform.Translate(new Vector2(speed, 0f) * transform.parent.localScale.x * Time.deltaTime);
+            if (collision.gameObject.CompareTag("MeleeWeapon"))
+            {
+                // Weapon will rotate in air we need to check if it points to left or right and apply speed correctly
+                if (collision.gameObject.transform.right.x < 0f)
+                {
+                    collision.transform.Translate(new Vector2(-speed, 0f) * transform.parent.localScale.x * Time.deltaTime);
+                }
+                else
+                {
+                    collision.transform.Translate(new Vector2(speed, 0f) * transform.parent.localScale.x * Time.deltaTime);
+                }
+            }
+            else
+                collision.transform.Translate(new Vector2(speed, 0f) * transform.parent.localScale.x * Time.deltaTime);
         }
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Change layer back to normal
+        if (collision.gameObject.CompareTag("Player") && !stopped)
+            rightEndPointCollider.gameObject.layer = LayerMask.NameToLayer("Default");
     }
 
     public void Stop()
