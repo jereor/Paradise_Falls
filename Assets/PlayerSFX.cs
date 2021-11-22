cdf3abe7a7dfacd57063941e175b5a11c525e034
TJere <56117300+TJere@users.noreply.github.com>
2021-11-22 13:04:13 +0200

    Audio mixer + footstep fixes

--- Assets/PlayerSFX.cs
+++ Assets/PlayerSFX.cs
@@ -7,7 +7,7 @@ public class PlayerSFX : MonoBehaviour
     [SerializeField] private AudioSource[] playerSteps;
     public void PlayRandomPlayerStepSound()
     {
-        int random = Random.Range(0, 5);
+        int random = Random.Range(0, 1);
         playerSteps[random].Play();
     }
 }
